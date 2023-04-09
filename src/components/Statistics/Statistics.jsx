bbdda9f31f4c7aa46012c78e54b0ce5628a4e528
Abir <abirovi481@gmail.com>
2023-04-10 00:36:37 +0600

    fourth-commit

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,46 @@
 import React from 'react';
+import { PieChart, Pie, Cell, Tooltip } from 'recharts';
 
+  
+const data = [
+    { name: 'Assignment-1', value: 57 },
+    { name: 'Assignment-2', value: 59 },
+    { name: 'Assignment-3', value: 60 },
+    { name: 'Assignment-4', value: 60 },
+    { name: 'Assignment-5', value: 60 },
+    { name: 'Assignment-6', value: 55 },
+    { name: 'Assignment-7', value: 57 },
+    { name: 'Assignment-8', value: 60 },
+  ];
+  
+  const COLORS = ['#FF6384', '#36A2EB', '#f54266', '#2ECC71', '#F1C40F', '#E74C3C', '#8E44AD', '#7E90FE'];
+  
 const Statistics = () => {
+
     return (
-        <div>
-            
-        </div>
+        <div className='container d-flex justify-content-center mt-4'>
+           <div>
+            <h1>My Assignment Numbers</h1>
+            <PieChart width={400} height={400}>
+                        <Pie
+                            data={data}
+                            dataKey="value"
+                            nameKey="name"
+                            cx="50%"
+                            cy="50%"
+                            innerRadius={60}
+                            outerRadius={120}
+                            fill="#8884d8"
+                            label
+                        >
+                            {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                    <Tooltip />
+                </PieChart>
+           </div>
+      </div>
     );
 };
 
--- src/components/featuredJobs/FeaturedJobs.jsx
+++ src/components/featuredJobs/FeaturedJobs.jsx
@@ -4,19 +4,19 @@ const FeaturedJobs = (props) => {
     const {name, image, jobPost} = props.company
     return (
         <div>
-            <div class="col">
-                <div style={{border:"none"}} class="card shadow p-3 h-100">
-                <img style={{width:"150px", height:"60px"}} src={image} class="card-img-top ps-2" alt="..."/>
-                <div style={{height:"250px"}} class="card-body mt-2">
+            <div className="col">
+                <div style={{border:"none"}} className="card shadow p-3 h-100">
+                <img style={{width:"150px", height:"60px"}} src={image} className="card-img-top ps-2" alt="..."/>
+                <div style={{height:"250px"}} className="card-body mt-2">
                     <h4 className='card-title'>{jobPost.name}</h4>
-                    <h6 class="card-text text-secondary">{name}</h6>
+                    <h6 className="card-text text-secondary">{name}</h6>
                     <div className='my-4'>
                         <span style={{border:"1px solid #7E90FE", color:"#7E90FE"}} className='me-2 p-2 rounded  '>{jobPost.work}</span>
                         <span style={{border:"1px solid #7E90FE", color:"#7E90FE"}} className='p-2  rounded'>Full Time</span>
                     </div>
                     <div className='d-flex gap-4'>
-                       <p class="card-text">{jobPost.location}</p>
-                       <p class="card-text">Salary: {jobPost.salary}</p>
+                       <p className="card-text">{jobPost.location}</p>
+                       <p className="card-text">Salary: {jobPost.salary}</p>
                     </div>
                     <button style={{background: "linear-gradient(90deg, #7E90FE 0%, #9873FF 100%)"}} className='btn text-white'>View details</button>
                 </div>
