7b090c7967bf2a8c2b93e69f4a9213f4cd1bab21
Abir <abirovi481@gmail.com>
2023-04-09 21:42:10 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './components/Layout/Main';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import Blogs from './components/Blogs/Blogs';
+import Error from './components/ErrorPage/Error';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <Error></Error>,
+    children:[
+      { 
+        path: '/',
+        element: <Home></Home>
+      },
+      { 
+        path: '/statistics',
+        element: <Statistics></Statistics>
+      },
+      { 
+        path: '/applied-jobs',
+        element: <AppliedJobs></AppliedJobs>
+      },
+      { 
+        path: '/blog',
+        element: <Blogs></Blogs>
+      },
+    ]
+  },
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
