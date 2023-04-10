a71a1e1c8bc6195869a7dd9f3e917e46616dd98c
Abir <abirovi481@gmail.com>
2023-04-10 17:27:37 +0600

    seventh-commit

--- src/components/ErrorPage/Error.jsx
+++ src/components/ErrorPage/Error.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Error = () => {
     return (
-        <div>
-            <h1>dkjnhfrhys</h1>
+        <div className='container mt-5 text-center'>
+            <img src="img/error.png" alt="" />
+            <h1 className='mb-2'>404 Error</h1>
+            <h3 className='my-2 text-danger'>Ouch! Page not found</h3>
         </div>
     );
 };
